32551658b40cbad49f5dc20c494d822e1753c988
廖政 <813144314@qq.com>
2022-02-27 23:28:09 +0800

    14-easy-first

---
+++ gymnastics/14-easy-first/template.ts
@@ -0,0 +1,3 @@
+// type First<T extends any[]> = T['length'] extends 0 ? never : T[0]
+// type First<T extends any[]> = T extends [] ? never : T[0]
+type First<T extends any[]> = T[0] extends T[number] ? T[0] : never
